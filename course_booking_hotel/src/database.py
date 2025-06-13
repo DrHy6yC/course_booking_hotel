a87d6a715a625b51f576e765ef6963e0b13cec24
denisigorevich <dr.hy6yc@gmail.com>
2025-06-13 08:48:50 +0300

    Добавил асинхронного создателя сессий к БД

--- course_booking_hotel/src/database.py
+++ course_booking_hotel/src/database.py
@@ -4,4 +4,6 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.config import settings
 
 engine = create_async_engine(url=settings.DB_URL)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False
+                                         )
 
