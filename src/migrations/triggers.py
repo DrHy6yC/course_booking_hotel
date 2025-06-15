2566ae55537b321a9835740b81c30490bf1090e3
Denis Plaksin <dr.hy6yc@gmail.com>
2025-06-15 11:57:13 +0300

    Добавил триггер при создании пользователя записывалась дата и время регистрации в поле created_at

---
+++ src/migrations/triggers.py
@@ -0,0 +1,18 @@
+def create_trigger_function(table_name, column):
+    return f"""
+CREATE OR REPLACE FUNCTION set_{table_name}_{column}()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.{column} = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+"""
+
+def create_update_trigger(table_name, column, type_before):
+    return f"""
+    CREATE TRIGGER {table_name}_{type_before}_timestamp
+    BEFORE {type_before} ON {table_name}
+    FOR EACH ROW
+    EXECUTE FUNCTION set_{table_name}_{column}();
+    """
---
+++ src/migrations/versions/2025_06_15_1123-8a86ef1529a7_добавление_триггера_в_таблицу_users_для_.py
@@ -0,0 +1,33 @@
+"""Добавление триггера в таблицу users для поля created_at
+
+Revision ID: 8a86ef1529a7
+Revises: 019f2544a3d3
+Create Date: 2025-06-15 11:23:27.322163
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+
+from src.migrations.triggers import create_trigger_function, create_update_trigger
+
+# revision identifiers, used by Alembic.
+revision: str = "8a86ef1529a7"
+down_revision: Union[str, None] = "019f2544a3d3"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+table_name = "users"
+column = "created_at"
+type_before = "INSERT"
+
+def upgrade() -> None:
+    op.execute(create_trigger_function(table_name, column))
+    op.execute(create_update_trigger(table_name,column, type_before))
+
+
+def downgrade() -> None:
+    op.execute(f"DROP TRIGGER IF EXISTS {table_name}_{type_before}_timestamp ON {table_name};")
+    op.execute(f"DROP FUNCTION IF EXISTS set_{table_name}_{column};")
