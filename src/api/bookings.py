90f51b8c00549b7e7f1b58b7f4b5e9a67eda8b20
Denis Plaksin <dr.hy6yc@gmail.com>
2025-06-20 17:10:42 +0300

    Рефакторинг кода, перенес get ручки в начало файла

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -7,28 +7,6 @@ from src.schemas.booking import BookingAdd, BookingRequestAdd
 router = APIRouter(prefix="/bookings", tags=["Бронирование номеров"])
 
 
-@router.post(
-    path="",
-    summary="Забронировать номер",
-    description="Забронировать номер для активного пользователя"
-)
-async def create_bookings(
-        db: DBDep,
-        user_id: UserIdDep,
-        booking_data: BookingRequestAdd,
-):
-    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
-    if not room:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail={"status": "Error - Номер не найден"}
-        )
-    _booking_data = BookingAdd(user_id=user_id, price=room.price, **booking_data.model_dump())
-    result = await db.bookings.add(_booking_data)
-    await db.commit()
-    return  {"status": "OK", "booking": result}
-
-
 @router.get(
     path="",
     summary="Получить список всех забронированных номеров",
@@ -50,3 +28,25 @@ async def get_bookings_me(
         user_id: UserIdDep
 ):
     return await db.bookings.get_filter_by(user_id=user_id)
+
+
+@router.post(
+    path="",
+    summary="Забронировать номер",
+    description="Забронировать номер для активного пользователя"
+)
+async def create_bookings(
+        db: DBDep,
+        user_id: UserIdDep,
+        booking_data: BookingRequestAdd,
+):
+    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    if not room:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"status": "Error - Номер не найден"}
+        )
+    _booking_data = BookingAdd(user_id=user_id, price=room.price, **booking_data.model_dump())
+    result = await db.bookings.add(_booking_data)
+    await db.commit()
+    return  {"status": "OK", "booking": result}
