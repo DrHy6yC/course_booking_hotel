de6eb83b6703aef8585b0ecd12dc9bebf08c6c9d
Denis Plaksin <dr.hy6yc@gmail.com>
2025-06-21 17:35:08 +0300

    Пофиксил свой запрос, заменил на тот что в курсе

--- src/repositories/utils.py
+++ src/repositories/utils.py
@@ -11,26 +11,12 @@ def unoccupied_rooms(
         date_to: date,
         hotel_id: int | None = None
 ):
-    rooms_hotel = (
-        select(RoomsORM.id.label(name="rooms_id"))
-        .select_from(RoomsORM)
-    )
-
-    if hotel_id is not None:
-        rooms_hotel = rooms_hotel.filter(RoomsORM.hotel_id == hotel_id)
-
-    rooms_hotel = rooms_hotel.cte(name="rooms_hotel")
-
     rooms_count = (
-        select(BookingsORM.room_id, func.count("*").label(name="rooms_booked"))
+        select(BookingsORM.room_id, func.count("*").label("rooms_booked"))
         .select_from(BookingsORM)
         .filter(
-            BookingsORM.date_from >= date_from,
-            BookingsORM.date_to <= date_to,
-            BookingsORM.room_id.in_(
-                select(rooms_hotel.c.rooms_id)
-                .select_from(rooms_hotel)
-            )
+            BookingsORM.date_from <= date_to,
+            BookingsORM.date_to >= date_from,
         )
         .group_by(BookingsORM.room_id)
         .cte(name="rooms_count")
@@ -38,15 +24,34 @@ def unoccupied_rooms(
 
     rooms_left_table = (
         select(
-            RoomsORM.id.label(name="rooms_id"),
-            (RoomsORM.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)).label(name="rooms_left")
+            RoomsORM.id.label("room_id"),
+            (RoomsORM.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)).label("rooms_left"),
         )
-        .select_from(rooms_count)
-        .outerjoin(RoomsORM, RoomsORM.id == rooms_count.c.room_id)
+        .select_from(RoomsORM)
+        .outerjoin(rooms_count, RoomsORM.id == rooms_count.c.room_id)
         .cte(name="rooms_left_table")
     )
 
+    rooms_ids_for_hotel = (
+        select(RoomsORM.id)
+        .select_from(RoomsORM)
+    )
+    if hotel_id is not None:
+        rooms_ids_for_hotel = rooms_ids_for_hotel.filter_by(hotel_id=hotel_id)
 
-    query = select(rooms_left_table.c.rooms_id).select_from(rooms_left_table).filter(rooms_left_table.c.rooms_left > 0)
+    rooms_ids_for_hotel = (
+        rooms_ids_for_hotel
+        .subquery(name="rooms_ids_for_hotel")
+    )
+
+    query = (
+        select(rooms_left_table.c.room_id)
+        .select_from(rooms_left_table)
+        .filter(
+            rooms_left_table.c.rooms_left > 0,
+            rooms_left_table.c.room_id.in_(rooms_ids_for_hotel),
+        )
+    )
+
     print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
     return query
