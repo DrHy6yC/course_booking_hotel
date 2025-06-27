3fd0308620a7cb92e5f3093f45afb25a7225d0b1
Denis Plaksin <dr.hy6yc@gmail.com>
2025-06-27 14:56:48 +0300

    Добавил эндпоинт POST /tasks/images

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+import shutil
 
-from src.tasks.tasks import sleep_task
+from fastapi import APIRouter, UploadFile
+
+from src.tasks.tasks import sleep_task, resize_image
 
 router = APIRouter(prefix="/tasks", tags=["Задачи"])
 
@@ -12,4 +14,17 @@ router = APIRouter(prefix="/tasks", tags=["Задачи"])
 )
 async def create_task():
     sleep_task.delay()
+    return {"status": "OK"}
+
+
+@router.post(
+    path="/images",
+    summary="Добавить картинку",
+    description="Добавляем картинку в папку в static/images"
+)
+def add_image(file: UploadFile):
+    image_path = f"src/static/images/{file.filename}"
+    with open(image_path, "wb+") as new_file:
+        shutil.copyfileobj(fsrc=file.file, fdst=new_file)
+    resize_image.delay(image_path)
     return {"status": "OK"}
