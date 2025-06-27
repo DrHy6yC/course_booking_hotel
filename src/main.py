f6e654036c8638d058891f39a64416241ac2455b
Denis Plaksin <dr.hy6yc@gmail.com>
2025-06-27 16:09:25 +0300

    Добавил пример, как можно запускать переодическую(бесконечную задачу без Celery

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -8,7 +9,11 @@ import uvicorn
 import sys
 from pathlib import Path
 
+
 sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.dependencies import get_db
+from src.connectors.redis_init import redis_manager
 
 from src.api.auth import router as router_auth
 from src.api.bookings import router as router_bookings
@@ -16,12 +21,22 @@ from src.api.facilities import router as router_facilities
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
 from src.api.tasks import router as router_tasks
-from src.connectors.redis_init import redis_manager
 
+
+async def get_bookings_with_today_checkin_helper():
+    async for db in get_db():
+        bookings_get = await db.bookings.get_bookings_with_today_checkin()
+        print(f"\n{bookings_get}\n")
+
+async def run_send_email_regularly():
+    while True:
+        await get_bookings_with_today_checkin_helper()
+        await asyncio.sleep(5)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    asyncio.create_task(run_send_email_regularly())
     await redis_manager.connect()
     FastAPICache.init(
         RedisBackend(redis_manager.redis),
